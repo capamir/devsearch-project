e3c40ba0d49bc36d8198086f5727d489640c8aa1
amirhossein <79211332+capamir@users.noreply.github.com>
2023-02-26 16:53:21 -0500

    projects app class base view

--- projects/urls.py
+++ projects/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projects, name='projects'),
-    path('create-project/', views.createProject, name='create-project'),
-    path('<str:pk>/', views.project, name='single-project'),
-    path('update-project/<str:pk>/', views.updateProject, name='update-project'),
-    path('delete-project/<str:pk>/', views.deleteProject, name='delete-project'),
+    path('', views.ProjectsView.as_view(), name='projects'),
+    path('create-project/', views.ProjectCreateView.as_view(), name='create-project'),
+    path('<str:pk>/', views.ProjectView.as_view(), name='single-project'),
+    path('update-project/<str:pk>/', views.ProjectUpdateView.as_view(), name='update-project'),
+    path('delete-project/<str:pk>/', views.ProjectDeleteView.as_view(), name='delete-project'),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,111 +1,136 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from .utils import searchProjects, paginateProjects
 
 # Create your views here.
-def projects(request):
-    projects, search_query = searchProjects(request)
+class ProjectsView(View):
+    template_name = 'projects/projects.html'
+    def get(self, request):
+        projects, search_query = searchProjects(request)
+        custom_range, projects = paginateProjects(request, projects, 3)
+        
+        context  = {
+            'projects': projects,
+            'search_query': search_query,
+            'custom_range': custom_range
+        }
+        return render(request, self.template_name, context)
+
+
+class ProjectView(View):
+    template_name = 'projects/single-project.html'
+    form_class = ReviewForm
     
-    custom_range, projects = paginateProjects(request, projects, 3)
+    def dispatch(self, request, *args, **kwargs):
+        self.project_obj = Project.objects.get(pk=kwargs['pk'])
+        self.reviews = self.project_obj.review_set.all()
 
+        return super().dispatch(request, *args, **kwargs)
     
-    context = {
-        'projects': projects,
-        'search_query': search_query,
-        'custom_range': custom_range
-    }
-    return render(request, 'projects/projects.html', context)
 
-def project(request, pk):
-    projectObj = Project.objects.get(id=pk)
-    form = ReviewForm()
-    reviews = projectObj.review_set.all()
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
 
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        context = {
+            'project': self.project_obj,
+            'form': form,
+            'reviews': self.reviews,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
-            review.project = projectObj
+            review.project = self.project_obj
             review.owner = request.user.profile
             review.save()
-            #Update the project's vote count
-            projectObj.getVoteCount
-            
+            self.project_obj.getVoteCount
+
             messages.success(request, 'Review successfully was submitted!')
-            return redirect('single-project', pk=projectObj.id)
+            return redirect('single-project', pk=self.project_obj.id)
+        
+        else:
+            context = {
+                'project': self.project_obj,
+                'form': form,
+                'reviews': self.reviews,
+            }
+            return render(request, self.template_name, context)
+    
 
-    context = {
-        'project': projectObj,
-        'form': form,
-        'reviews': reviews,
-    }
-    return render(request, 'projects/single-project.html', context)
+class ProjectCreateView(LoginRequiredMixin, View):
+    template_name = 'projects/project-form.html'
+    form_class = ProjectForm
 
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {'form': form}
+        return render(request, self.template_name, context)
 
-@login_required(login_url='login')
-def createProject(request):
-    profile = request.user.profile
-    form = ProjectForm()
-
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         newtags = request.POST.get('newtags').replace(',',  " ").split()
-
-        form = ProjectForm(request.POST, request.FILES)
+        
+        form = self.form_class(request.POST)
         if form.is_valid():
             project = form.save(commit=False)
-            project.owner = profile
-            project.save()
+            project.owner = request.user.profile
+            project.save() 
 
             for tag in newtags:
                 tag, created = Tag.objects.get_or_create(name=tag)
                 project.tags.add(tag)
 
             return redirect('account')
-
-    context = {
-        'form': form
-    }
-    return render(request, 'projects/project-form.html', context)
+        else:
+            context = {'form': form}
+            return render(request, self.template_name, context)
 
 
-@login_required(login_url='login')
-def updateProject(request, pk):
-    profile = request.user.profile
-    projectObj = profile.project_set.get(id=pk)
-    form = ProjectForm(instance=projectObj)
+class ProjectUpdateView(LoginRequiredMixin, View):
+    template_name = 'projects/project-form.html'
+    form_class = ProjectForm
 
-    if request.method == 'POST':
-        newtags = request.POST.get('newtags').replace(',',  " ").split()
-
-        form = ProjectForm(request.POST, request.FILES, instance=projectObj)
-        if form.is_valid():
-            project = form.save()
-            for tag in newtags:
-                tag, created = Tag.objects.get_or_create(name=tag)
-                project.tags.add(tag)
-
-            return redirect('account')
-
-    context = {
-        'form': form,
-        'project': projectObj
-    }
-    return render(request, 'projects/project-form.html', context)
-
-
-@login_required(login_url='login')
-def deleteProject(request, pk):
-    profile = request.user.profile
-    project = profile.project_set.get(id=pk)
-    
-    if request.method == 'POST':
-        project.delete()
-        return redirect('account')
+    def dispatch(self, request, *args, **kwargs):
+        profile = request.user.profile
+        self.project_obj = profile.project_set.get(id=kwargs['pk'])
         
-    context = {
-        'project': project
-    }
-    return render(request, 'delete-template.html', context)
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(instance=self.project_obj)
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
+        form = self.form_class(request.POST, request.FILES, instance=self.project_obj)
+        if form.is_valid():
+            project = form.save(commit=False)
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+
+            return redirect('account')
+        else:
+            context = {'form': form}
+            return render(request, self.template_name, context)
+        
+class ProjectDeleteView(LoginRequiredMixin, View):
+    template_name = 'delete-template.html'
+    def dispatch(self, request, *args, **kwargs):
+        profile = request.user.profile
+        self.project = profile.project_set.get(id=kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+        
+    def get(self, request, *args, **kwargs):
+        context = {'project': self.project}
+        return render(request, 'delete-template.html', context)
+
+    def post(self, request, *args, **kwargs):
+        self.project.delete()
+        return redirect('account')
