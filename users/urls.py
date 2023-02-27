ac7ffaacf4d712a5a8b0b91aef8172859a47bf29
amirhossein <79211332+capamir@users.noreply.github.com>
2023-02-27 16:17:29 -0500

    users app class base view

--- users/urls.py
+++ users/urls.py
@@ -2,23 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginUser, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerUser, name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
 
-    path('account/', views.userAccount, name='account'),
-    path('edit-account/', views.editAccount, name='edit-account'),
-    path('inbox/', views.inbox, name='inbox'),
+    path('account/', views.UserAccountView.as_view(), name='account'),
+    path('edit-account/', views.UserEditAccountView.as_view(), name='edit-account'),
+    path('inbox/', views.InboxView.as_view(), name='inbox'),
 
-    path('create-skill/', views.createSkill, name='create-skill'),
+    path('create-skill/', views.SkillCreateView.as_view(), name='create-skill'),
 
-    path('', views.profiles, name='profiles'),
-    path('profile/<str:pk>/', views.profilePage, name='single-profile'),
+    path('', views.ProfilesView.as_view(), name='profiles'),
+    path('profile/<str:pk>/', views.ProfileDetailView.as_view(), name='single-profile'),
 
-    path('update-skill/<str:pk>/', views.updateSkill, name='update-skill'),
-    path('delete-skill/<str:pk>/', views.deleteSkill, name='delete-skill'),
+    path('update-skill/<str:pk>/', views.SkillUpdateView.as_view(), name='update-skill'),
+    path('delete-skill/<str:pk>/', views.SkillDeleteView.as_view(), name='delete-skill'),
 
-    path('message/<str:pk>/', views.viewMessage, name='message'),
-    path('send-message/<str:pk>/', views.createMessage, name='send-message'),
+    path('message/<str:pk>/', views.MessageView.as_view(), name='message'),
+    path('send-message/<str:pk>/', views.MessageCreateView.as_view(), name='send-message'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from .models import Profile
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
@@ -9,13 +10,21 @@ from .utills import searchProfiles, paginateProfiles
 
 # Create your views here.
 # ---------------------Login & Register -----------------------------
-def loginUser(request):
+class UserLoginView(View):
     page = 'login'
+    template_name = 'users/login-register.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('profiles')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        context = {'page': self.page}
+        return render(request, self.template_name, context)
 
-    if request.user.is_authenticated:
-        return redirect('profiles')
 
-    if request.method == 'POST':
+    def post(self, request, *args, **kwargs):
         username = request.POST['username'].lower()
         password = request.POST['password']
 
@@ -33,23 +42,37 @@ def loginUser(request):
         else:
             messages.error(request, 'Username OR password is incorrect')
 
+        context = {'page': self.page}
+        return render(request, self.template_name, context)
 
-    context = {
-        'page': page
-    }
-    return render(request, 'users/login-register.html', context)
 
-def logoutUser(request):
-    logout(request)
-    messages.info(request, 'User was logged out!')
-    return redirect('profiles')
+class UserLogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, 'User was logged out!')
+        return redirect('profiles')
 
-def registerUser(request):
+
+class UserRegistrationView(View):
     page = 'register'
-    form = CustomUserCreationForm()
+    template_name = 'users/login-register.html'
+    form_class = CustomUserCreationForm
 
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('profiles')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {
+            'page': self.page,
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -59,176 +82,222 @@ def registerUser(request):
 
             login(request, user)
             return redirect('edit-account')
+        else:
+            context = {
+                'page': self.page,
+                'form': form,
+            }
+            return render(request, self.template_name, context)
 
-    context = {
-        'page': page,
-        'form': form,
-    }
-    return render(request, 'users/login-register.html', context)
 
 # --------------------Profile Read ------------------------------------
-def profiles(request):
-    profiles, search_query = searchProfiles(request)
+class ProfilesView(View):
+    template_name = 'users/profiles.html'
+
+    def get(self, request):
+        profiles, search_query = searchProfiles(request)
     
-    custom_range, profiles = paginateProfiles(request, profiles, 3)
+        custom_range, profiles = paginateProfiles(request, profiles, 3)
 
-    context = {
-        'profiles': profiles,
-        'search_query': search_query,
-        'custom_range': custom_range,
-    }
-    return render(request, 'users/profiles.html', context)
+        context = {
+            'profiles': profiles,
+            'search_query': search_query,
+            'custom_range': custom_range,
+        }
+        return render(request, self.template_name, context)
 
-def profilePage(request, pk):
-    profileObj = Profile.objects.get(id=pk)
 
-    if request.user.profile == profileObj:
-        return redirect('account')
+class ProfileDetailView(View):
+    template_name = 'users/user-profile.html'
+    def dispatch(self, request, *args, **kwargs):
+        self.profileObj = Profile.objects.get(id=kwargs['pk'])
+        
+        if request.user.profile == self.profileObj:
+            return redirect('account')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        topSkills = self.profileObj.skill_set.exclude(description__exact="")
+        otherSkills = self.profileObj.skill_set.filter(description="")
 
-    topSkills = profileObj.skill_set.exclude(description__exact="")
-    otherSkills = profileObj.skill_set.filter(description="")
-
-    context = {
-        'profile': profileObj,
-        'topSkills': topSkills,
-        'otherSkills': otherSkills
-    }
-    return render(request, 'users/user-profile.html', context)
+        context = {
+            'profile': self.profileObj,
+            'topSkills': topSkills,
+            'otherSkills': otherSkills
+        }
+        return render(request, self.template_name, context)
 
 # ------------------Account Read & Update ------------------------------
-@login_required(login_url='login')
-def userAccount(request):
-    profile = request.user.profile
+class UserAccountView(LoginRequiredMixin, View):
+    template_name = 'users/account.html'
 
-    skills = profile.skill_set.all()
-    projects = profile.project_set.all()
+    def get(self, request):
+        profile = request.user.profile
 
-    context = {
-        'profile': profile,
-        'skills': skills,
-        'projects': projects,
-    }
-    return render(request, 'users/account.html', context)
+        skills = profile.skill_set.all()
+        projects = profile.project_set.all()
 
-@login_required(login_url='login')
-def editAccount(request):
-    profile = request.user.profile
-    form = ProfileForm(instance=profile)
+        context = {
+            'profile': profile,
+            'skills': skills,
+            'projects': projects,
+        }
+        return render(request, self.template_name, context)
 
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES, instance=profile)
+
+class UserEditAccountView(LoginRequiredMixin, View):
+    template_name = 'users/profile-form.html'
+    form_class = ProfileForm
+    def dispatch(self, request, *args, **kwargs):
+        self.profile = request.user.profile
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request):
+        form = self.form_class(instance=self.profile)
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES, instance=self.profile)
         if form.is_valid():
             form.save()
 
             return redirect('account')
-
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/profile-form.html', context)
-
+        else:
+            context = {
+                'form': form,
+            }
+            return render(request, self.template_name, context)
 
 # ------------------Skill Create & Update Delete ------------------------
+class SkillCreateView(LoginRequiredMixin, View):
+    template_name = 'users/skill-form.html'
+    form_class = SkillForm
 
-@login_required(login_url='login')
-def createSkill(request):
-    profile = request.user.profile
-    form = SkillForm()
+    def get(self, request):
+        form = self.form_class()
+        context = {'form': form,}
+        return render(request, self.template_name, context)
 
-    if request.method == 'POST':
-        form = SkillForm(request.POST)
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             skill = form.save(commit=False)
-            skill.owner = profile
+            skill.owner = request.user.profile
             skill.save()
 
             messages.success(request, 'Skill was successfully added')
             return redirect('account')
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/skill-form.html', context)
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
 
-@login_required(login_url='login')
-def updateSkill(request, pk):
-    profile = request.user.profile
-    skill = profile.skill_set.get(id=pk)
-    form = SkillForm(instance=skill)
 
-    if request.method == 'POST':
-        form = SkillForm(request.POST, instance=skill)
+class SkillUpdateView(LoginRequiredMixin, View):
+    template_name = 'users/skill-form.html'
+    form_class = SkillForm
+
+    def dispatch(self, request, *args, **kwargs):
+        profile = request.user.profile
+        self.skill = profile.skill_set.get(id=kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(instance=self.skill)
+
+        context = {'form': form,}
+        return render(request, 'users/skill-form.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        form = SkillForm(request.POST, instance=self.skill)
         if form.is_valid():
             form.save()
-
             messages.success(request, 'Skill was successfully updated')
             return redirect('account')
+        
+        else:
+            context = {'form': form,}
+            return render(request, 'users/skill-form.html', context)
+        
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/skill-form.html', context)
+class SkillDeleteView(LoginRequiredMixin, View):
+    template_name = 'users/delete-template.html'
 
-@login_required(login_url='login')
-def deleteSkill(request, pk):
-    profile = request.user.profile
-    skill = profile.skill_set.get(id=pk)
+    def dispatch(self, request, *args, **kwargs):
+        profile = request.user.profile
+        self.skill = profile.skill_set.get(id=kwargs['pk'])
 
-    if request.method == 'POST':
-        skill.delete()
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        context = {'object': self.skill,}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        self.skill.delete()
         messages.success(request, 'Skill deleted')
         return redirect('account')
 
-    context = {
-        'object': skill,
-    }
-    return render(request, 'users/delete-template.html', context)
-
-
 # ------------------Inbox Create & Update Delete ------------------------
-
-@login_required(login_url='login')
-def inbox(request):
-    profile = request.user.profile
-    messageRequests = profile.messages.all()
-    unreadCount = messageRequests.filter(is_read=False).count()
-
-    context = {
-        'messageRequests': messageRequests,
-        'inreadCount': unreadCount,
-
-    }
-    return render(request, 'users/inbox.html', context)
-
-@login_required(login_url='login')
-def viewMessage(request, pk):
-    profile = request.user.profile
-    message = profile.messages.get(id=pk)
-    if message.is_read == False:
-        message.is_read = True
-        message.save()
-
-    context = {
-        'message': message,
-    }
-    return render(request, 'users/message.html', context)
-
-
-def createMessage(request, pk):
-    recipient = Profile.objects.get(id=pk)
-    form = MessageForm()
-    # Sender
-    try:
+class InboxView(LoginRequiredMixin, View):
+    template_name = 'users/inbox.html'
+    def get(self, request, *args, **kwargs):
         profile = request.user.profile
-    except:
-        profile = None
+        messageRequests = profile.messages.all()
+        unreadCount = messageRequests.filter(is_read=False).count()
 
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
+        context = {
+            'messageRequests': messageRequests,
+            'inreadCount': unreadCount,
+        }
+        return render(request, self.template_name, context)    
+
+
+class MessageView(LoginRequiredMixin, View):
+    template_name = 'users/message.html'
+    def get(self, request, *args, **kwargs):
+        profile = request.user.profile
+        message = profile.messages.get(id=kwargs['pk'])
+        if message.is_read == False:
+            message.is_read = True
+            message.save()
+
+        context = {'message': message,}
+        return render(request, 'users/message.html', context)
+
+
+class MessageCreateView(View):
+    template_name = 'users/message-form.html'
+    form_class = MessageForm
+
+    def dispatch(self, request, *args, **kwargs):
+        self.recipient = Profile.objects.get(id=kwargs['pk'])
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        context = {
+            'form': form,
+            'recipient': self.recipient,
+        }
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        try:
+            profile = request.user.profile
+        except:
+            profile = None
+
         if form.is_valid():
             message = form.save(commit=False)
             message.sender = profile
-            message.recipient = recipient
+            message.recipient = self.recipient
             if profile:
                 message.name = profile.name
                 message.email = profile.email
@@ -236,10 +305,11 @@ def createMessage(request, pk):
             message.save()
             
             messages.success(request, 'Your message was successfully sent.')
-            return redirect('single-profile', pk=recipient.id)
+            return redirect('single-profile', pk=self.recipient.id)
 
-    context = {
-        'form': form,
-        'recipient': recipient,
-    }
-    return render(request, 'users/message-form.html', context)
+        else:
+            context = {
+                'form': form,
+                'recipient': self.recipient,
+            }
+            return render(request, self.template_name, context)
